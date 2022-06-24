dba7297207dbc39cd230940a8963eeef472a8b70
Felipe Silva <flv.silva@yahoo.com.br>
2022-06-24 18:12:11 -0300

    Crição de estrutura do projeto
    * Adicionando suporte mvn/spring
    * Adicionando estrutura inicial/dependencias

---
+++ testeCrud/src/test/java/com/agrotis/testeCrud/TesteCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis.testeCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
